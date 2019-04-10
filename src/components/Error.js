76d8a2d827082155177b56464e8d6505be7f0734
Kevin Wang <kw1017@gmail.com>
2019-04-09 22:45:38 -0700

    add error page

---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Error = () => {
+
+    return (
+        <div>
+            <p>Error: Path doe not exist!!!</p>
+        </div>
+    );
+
+};
+
+
+ export default Error;
