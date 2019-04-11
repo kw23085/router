7587fcc9f076520f90a677f50296d16b3e9c0086
Kevin Wang <kw1017@gmail.com>
2019-04-10 22:21:18 -0700

    add navlink to pages

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import Navigation from "./components/Navigation";
 
 class App extends Component {
   render() {
     return (
 
       <BrowserRouter>
-        <Switch>
-          <Route path="/" component= {Home} exact />
-          <Route path="/about" component= {About} />
-          <Route path="/contact" component= {Contact} />
-          <Route component={Error} />
-        </Switch>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path="/" component= {Home} exact />
+            <Route path="/about" component= {About} />
+            <Route path="/contact" component= {Contact} />
+            <Route component={Error} />
+          </Switch>
+        </div>
       </BrowserRouter>
 
     );
